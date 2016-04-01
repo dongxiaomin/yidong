a9517ddba4153a4eb29dd26ab47b52cd753478f3
董小敏 <1344091359@qq.com>
2016-04-01 22:58:28 +0800

    修改

--- js/tab.js
+++ js/tab.js
@@ -1,49 +1,98 @@
-// window.onload=function(){
-//    var downMenu=document.getElementsByClassName("downMenu");
-//    var navbar=document.getElementsByClassName("navbar");
-//    var lis=document.getElementsByClassName("dropdown");
-   
-//    for(var i=0;i<lis.length;i++){
-//      lis[i].index=i;
-//      lis[i].onmouseover=function(){
-//          for(var j=0;j<navbar.length;j++){
-//             downMenu[j].style.display="none";
-//          }
-//             downMenu[this.index].style.display="block";
-//      }
-//      lis[i].onmouseout=function(){
-//          for(var j=0;j<lis.length;j++){
-//              downMenu[j].style.display="none";
-//          }
-//      }
-//    }
-// }
-
-
 window.onload=function(){
-  var win=document.getElementsByClassName(" banner_img")[0];
-  var imgs=document.getElementsByClassName("banner1");
-  //console.log(imgs.length);
-  var yuandians=document.getElementsByClassName("yuandian1");
-  //console.log(yuandians.length)
-  var num=0;
+//选项卡
+   var downMenu=document.getElementsByClassName("downMenu");
+   var navbar=document.getElementsByClassName("navbar");
+   var lis=document.getElementsByClassName("dropdown");
+   
+   for(var i=0;i<lis.length;i++){
+     lis[i].index=i;
+     lis[i].onmouseover=function(){
+         for(var j=0;j<navbar.length;j++){
+            downMenu[j].style.display="none";
+         }
+            downMenu[this.index].style.display="block";
+     }
+     lis[i].onmouseout=function(){
+         for(var j=0;j<lis.length;j++){
+             downMenu[j].style.display="none";
+         }
+     }
+   }
 
-  var t=setInterval(move,1000)
-  win.onmouseover=function(){
-    clearInterval(t);
+
+//轮播
+var wins=document.getElementsByClassName("banner_img")[0]  
+var imgs=document.getElementsByClassName("bannera")
+var btnL=document.getElementsByClassName("btnL")[0]
+var btnR=document.getElementsByClassName("btnR")[0]
+  // console.log(btnL)
+  // console.log(btnR)
+  var spans=document.getElementsByClassName("btnspan")
+  var num=0;
+  spans[0].style.background="red";
+  var t=setInterval(move,2000)
+  wins.onmouseover=function(){
+    clearInterval(t)
   }
-  win.onmouseout=function(){
-    t=setInterval(move,1000);
+  wins.onmouseout=function(){
+    t=setInterval(move,1000)
   }
- function move(){
-  num++;
-  if(num==imgs.length){
-    for(var i=0;i<imgs.length;i++){
-      imgs[i].style.zIndex=0;
-      yuandians[i].style.background="#fff"
+  for(var i=0;i<imgs.length;i++){
+    spans[i].index=i;
+    spans[i].onclick=function(){
+      num=this.index
+      for(var i=0;i<imgs.length;i++){
+        imgs[i].style["z-index"]=0;
+        spans[i].style.background="gray"
+      }
+      imgs[this.index].style["z-index"]=5;
+      spans[this.index].style.background="red" 
     }
-    imgs[num].style.zIndex=3;
-    yuandians[num].style.background="red";
   }
- } 
-}
+  btnR.onclick=function(){
+    moveR();
+  }
+  btnL.onclick=function(){
+    moveL()
+  }
+  function moveR(){
+    num++;
+    if(num==imgs.length){
+      num=0;
+    }
+    for(var i=0;i<imgs.length;i++){
+      imgs[i].style["z-index"]=0;
+      spans[i].style.background="gray"
+    } imgs[num].style["z-index"]=5;
+    spans[num].style.background="red"
+  }
+  
+  function moveL(){
+    num--
+    if(num<0){
+      num=imgs.length-1;
+    }
+    for(var i=0;i<imgs.length;i++){
+      imgs[i].style["z-index"]=0;
+      spans[i].style.background="gray"
+    } imgs[num].style["z-index"]=5;
+    spans[num].style.background="red"
+  }
+
+
+  function move(){
+    num++
+    if(num==imgs.length){
+      num=0;
+    }
+    for(var i=0;i<imgs.length;i++){
+      imgs[i].style["z-index"]=0;
+      spans[i].style.background="gray"
+    } imgs[num].style["z-index"]=5;
+    spans[num].style.background="red"
+  }
+  
+
+ }
+
+
